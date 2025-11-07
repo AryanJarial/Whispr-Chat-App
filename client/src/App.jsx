dfa49a99a3a5effd0927b155adbd41cc64a6380a
Aryan Jarial <aryanjarial77@gmail.com>
2025-11-07 16:52:41 +0530

    Added new components and set up background image

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,12 @@ import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage.jsx'
 import LoginPage from './pages/LoginPage'
 import ProfilePage from './pages/ProfilePage'
+import bgAry3 from './assets/bgAry3.jpg';
 
 const App = () => {
   return (
-    <div>
+    <div className="w-screen h-screen bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: `url(${bgAry3})` }}>
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path='/login' element={<LoginPage/>}/>
