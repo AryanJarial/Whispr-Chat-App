286f3951cf1cacf69f2912a471bd8fd90dddc8c4
Aryan Jarial <aryanjarial77@gmail.com>
2025-11-13 20:58:59 +0530

    feat: add database connection and integrate with server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import "dotenv/config";
 import cors from 'cors';
 import http from 'http';
+import { connect } from 'http2';
+import { connectDB } from './lib/db.js';
 
 //Create express app and HTTP server
 
@@ -14,6 +16,10 @@ app.use(express.json({limit: "4mb"}));
 app.use(cors());
 
 app.use('/api/status', (req,res)=> res.send('Server is live'));
+
+
+//Connect to mongodb
+await connectDB();
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`));
