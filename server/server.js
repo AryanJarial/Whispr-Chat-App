541f366f7d311ae9ca136a3f5acf22ad052b361b
Aryan Jarial <aryanjarial77@gmail.com>
2025-11-14 20:21:36 +0530

    feat: add user auth routes and integrate user controller endpoints

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import http from 'http';
 import { connect } from 'http2';
 import { connectDB } from './lib/db.js';
+import userRouter from './routes/userRoutes.js';
 
 //Create express app and HTTP server
 
@@ -15,7 +16,9 @@ const server = http.createServer(app)
 app.use(express.json({limit: "4mb"}));
 app.use(cors());
 
+//Routes Setup
 app.use('/api/status', (req,res)=> res.send('Server is live'));
+app.use('api/auth', userRouter);
 
 
 //Connect to mongodb
