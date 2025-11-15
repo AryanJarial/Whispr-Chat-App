0fad452f0e77a4aa4b9f651ce66687fb4cee773a
Aryan Jarial <aryanjarial77@gmail.com>
2025-11-15 09:27:06 +0530

    feat: add markMessageAsSeen function in message controller

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -47,3 +47,16 @@ export const getMessages = async(req,res) =>{
         res.json({success: false, message: error.message});
     }
 }
+
+//API to mark message as seen using message id
+
+export const markMessagesAsSeen = async (req,res)=>{
+    try {
+        const {id} = req.params;
+        await Message.findByIdAndUpdate(id, {seen:true});
+        res.json({success:true});
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message});
+    }
+}
