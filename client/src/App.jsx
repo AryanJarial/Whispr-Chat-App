7f25c332261c5e503aa8ed45096f09f76d92f2e0
Aryan Jarial <aryanjarial77@gmail.com>
2025-11-16 16:52:05 +0530

    feat: add AuthContext and setup toast notifications

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,18 @@ import HomePage from './pages/HomePage.jsx'
 import LoginPage from './pages/LoginPage'
 import ProfilePage from './pages/ProfilePage'
 import bgAry3 from './assets/bgAry3.jpg';
+import {Toaster} from 'react-hot-toast';
 
 const App = () => {
   return (
     <div className="w-screen h-screen bg-cover bg-center bg-no-repeat"
-      style={{ backgroundImage: `url(${bgAry3})` }}>
-      <Routes>
-        <Route path='/' element={<HomePage/>}/>
-        <Route path='/login' element={<LoginPage/>}/>
-        <Route path='/profile' element={<ProfilePage/>}/>
-      </Routes>
+      style={{ backgroundImage: `url(${bgAry3})` }}> 
+        <Toaster/>
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/login' element={<LoginPage/>}/>
+          <Route path='/profile' element={<ProfilePage/>}/>
+        </Routes> 
     </div>
   )
 }
