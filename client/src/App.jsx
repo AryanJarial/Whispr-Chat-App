bd54b30f7875a456fa1dac85b0e7faa7d209cf23
Aryan Jarial <aryanjarial77@gmail.com>
2025-11-17 12:49:23 +0530

    feat: implement auth-based routing in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage.jsx'
 import LoginPage from './pages/LoginPage'
 import ProfilePage from './pages/ProfilePage'
 import bgAry3 from './assets/bgAry3.jpg';
 import {Toaster} from 'react-hot-toast';
+import { AuthContext } from '../context/AuthContext.jsx'
 
 const App = () => {
+
+  const {authUser} = useContext(AuthContext);
+
   return (
     <div className="w-screen h-screen bg-cover bg-center bg-no-repeat"
       style={{ backgroundImage: `url(${bgAry3})` }}> 
         <Toaster/>
         <Routes>
-          <Route path='/' element={<HomePage/>}/>
-          <Route path='/login' element={<LoginPage/>}/>
-          <Route path='/profile' element={<ProfilePage/>}/>
+          <Route path='/' element={authUser ? <HomePage/>: <Navigate to='/login'/>}/>
+          <Route path='/login' element={!authUser ? <LoginPage/> : <Navigate to='/'/>}/>
+          <Route path='/profile' element={authUser ? <ProfilePage/> : <Navigate to='/login'/>}/>
         </Routes> 
     </div>
   )
